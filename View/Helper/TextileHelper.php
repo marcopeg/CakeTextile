3f105be6b6ead16e835d2aa0a798b4f12091930b
thepeg <marco.pegoraro@gmail.com>
2012-05-25 15:58:00 +0200

    add files

---
+++ View/Helper/TextileHelper.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * TextileHelper
+ *
+ * @author: Marco Pegoraro <marco.pegoraro@gmail.com>
+ * @info: http://movableapp.com/textile
+ */
+
+
+
+// Import vendor's library
+App::import( 'Vendor', 'Textile.classTextile' );
+
+class TextileHelper extends AppHelper {
+	
+	private $Textile = null;
+	
+	public function __construct( $_View, $_Options ) {
+		
+		parent::__construct( $_View, $_Options );
+		
+		$this->Textile = new Textile;
+		
+	}
+	
+	
+	
+	
+/**
+ * Public access to the internal textile engine.
+ */	
+	public function engine() { return $this->Textile; }
+	
+
+
+
+
+
+
+
+
+
+
+/**
+ * Parse a string using the Textile vendor's library
+ */	
+	public function parse( $str ) {
+		
+		return $this->Textile->TextileThis( $str);
+		
+	}
+	
+	
+	
+/**
+ * It parse a text but remove block tags who can't fit inside a title
+ */	
+	public function title( $str ) {
+		
+		$allowed_tags = '<a><b><i><u><em><strong><sup><sub><small>';
+		
+		return strip_tags( $this->parse($str), $allowed_tags );
+		
+	}
+	
+}
